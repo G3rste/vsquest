06067de4bccd58fad42be8c865d8e3899f0b73a9
G3rste <philippgerstenberg@web.de>
2022-01-18 20:00:45 +0100

    initial commit

---
+++ src/Systems/VsQuest.cs
@@ -0,0 +1,12 @@
+﻿using Vintagestory.API.Common;
+
+namespace VsQuest
+{
+    public class VsQuest : ModSystem
+    {
+        public override void Start(ICoreAPI api)
+        {
+            base.Start(api);
+        }
+    }
+}
