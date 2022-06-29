93e8404b9ac8fe20b4c56f645334567c311c5e2e
G3rste <philippgerstenberg@web.de>
2022-06-29 23:20:54 +0200

    add simple release script

---
+++ release.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+version=$(grep -o '"version":\s*"\([0-9]\.*\)*"' resources/modinfo.json | grep -o '\([0-9]\.*\)*')
+releasefile='bin/vsquest_v'$version'.zip'
+exampleversion=$(grep -o '"version":\s*"\([0-9]\.*\)*"' example/modinfo.json | grep -o '\([0-9]\.*\)*')
+examplereleasefile='bin/vsquestexample_v'$exampleversion'_vsquest_'$version'.zip'
+dotnet build -c release
+jar -cMf $examplereleasefile -C example .
+mv bin/vsquest.zip $releasefile
+gh release create --generate-notes 'v'$version $releasefile $examplereleasefile
