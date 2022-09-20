40df8a57cf9e7e6560cd68d3ffc5e250e9e00e8e
G3rste <philippgerstenberg@web.de>
2022-09-20 18:56:52 +0200

    add prefix v to version

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@
 version=$(grep -o '"version":\s*"\([0-9]\.*\)*"' resources/modinfo.json | grep -o '\([0-9]\.*\)*')
 releasefile='bin/vsquest_v'$version'.zip'
 exampleversion=$(grep -o '"version":\s*"\([0-9]\.*\)*"' example/modinfo.json | grep -o '\([0-9]\.*\)*')
-examplereleasefile='bin/vsquestexample_v'$exampleversion'_vsquest_'$version'.zip'
+examplereleasefile='bin/vsquestexample_v'$exampleversion'_vsquest_v'$version'.zip'
 dotnet build -c release
 jar -cMf $examplereleasefile -C example .
 mv bin/vsquest.zip $releasefile
