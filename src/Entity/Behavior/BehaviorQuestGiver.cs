f245570a4eeecc916a17459fd2f68dbfa3c7d300
G3rste <philippgerstenberg@web.de>
2025-03-24 18:30:44 +0100

    add support for entitybehaviorconversable

--- src/Entity/Behavior/BehaviorQuestGiver.cs
+++ src/Entity/Behavior/BehaviorQuestGiver.cs
@@ -6,6 +6,7 @@ using Vintagestory.API.Datastructures;
 using Vintagestory.API.MathTools;
 using Vintagestory.API.Server;
 using Vintagestory.API.Client;
+using Vintagestory.GameContent;
 
 namespace VsQuest
 {
@@ -42,39 +43,74 @@ namespace VsQuest
             }
         }
 
-        public override void OnInteract(EntityAgent byEntity, ItemSlot itemslot, Vec3d hitPosition, EnumInteractMode mode, ref EnumHandling handled)
+        public override void AfterInitialized(bool onFirstSpawn)
         {
-            base.OnInteract(byEntity, itemslot, hitPosition, mode, ref handled);
-            if (entity.Alive && entity.Api is ICoreServerAPI sapi && byEntity is EntityPlayer player && mode == EnumInteractMode.Interact && player.Controls.Sneak)
+            base.AfterInitialized(onFirstSpawn);
+            var bh = entity.GetBehavior<EntityBehaviorConversable>();
+            if (bh != null)
             {
-                var questSystem = sapi.ModLoader.GetModSystem<QuestSystem>();
-                var activeQuests = questSystem.getPlayerQuests(player.PlayerUID, sapi).FindAll(quest => quest.questGiverId == entity.EntityId);
-                var availableQuestIds = new List<string>();
-                foreach (var questId in quests)
+                bh.OnControllerCreated += (controller) =>
                 {
-                    var quest = questSystem.QuestRegistry[questId];
-                    var key = quest.perPlayer ? String.Format("lastaccepted-{0}-{1}", questId, (byEntity as EntityPlayer)?.PlayerUID) : String.Format("lastaccepted-{0}", questId);
-                    if (entity.WatchedAttributes.GetDouble(key, -quest.cooldown) + quest.cooldown < sapi.World.Calendar.TotalDays
-                            && activeQuests.Find(activeQuest => activeQuest.questId == questId && activeQuest.questGiverId == entity.EntityId) == null
-                            && predecessorsCompleted(quest, player.PlayerUID))
-                    {
-                        availableQuestIds.Add(questId);
-                    }
-                }
-                var message = new QuestInfoMessage()
-                {
-                    questGiverId = entity.EntityId,
-                    availableQestIds = availableQuestIds,
-                    activeQuests = activeQuests
+                    controller.DialogTriggers += Dialog_DialogTriggers;
                 };
-
-                sapi.Network.GetChannel("vsquest").SendPacket<QuestInfoMessage>(message, player.Player as IServerPlayer);
             }
         }
 
+        private int Dialog_DialogTriggers(EntityAgent triggeringEntity, string value, JsonObject data)
+        {
+            var behaviorConversable = entity.GetBehavior<EntityBehaviorConversable>();
+            behaviorConversable.Dialog?.TryClose();
+            if (value == "openquests" && triggeringEntity.Api is ICoreServerAPI sapi)
+            {
+                SendQuestInfoMessageToClient(sapi, triggeringEntity as EntityPlayer);
+                return 0;
+            }
+
+            return -1;
+        }
+
+
+        public override void OnInteract(EntityAgent byEntity, ItemSlot itemslot, Vec3d hitPosition, EnumInteractMode mode, ref EnumHandling handled)
+        {
+            if (entity.Alive
+                && entity.Api is ICoreServerAPI sapi
+                && byEntity is EntityPlayer player
+                && mode == EnumInteractMode.Interact
+                && player.Controls.Sneak
+                && !entity.HasBehavior<EntityBehaviorConversable>())
+            {
+                SendQuestInfoMessageToClient(sapi, player);
+            }
+        }
+
+        public void SendQuestInfoMessageToClient(ICoreServerAPI sapi, EntityPlayer player)
+        {
+            var questSystem = sapi.ModLoader.GetModSystem<QuestSystem>();
+            var activeQuests = questSystem.getPlayerQuests(player.PlayerUID, sapi).FindAll(quest => quest.questGiverId == entity.EntityId);
+            var availableQuestIds = new List<string>();
+            foreach (var questId in quests)
+            {
+                var quest = questSystem.QuestRegistry[questId];
+                var key = quest.perPlayer ? String.Format("lastaccepted-{0}-{1}", questId, player.PlayerUID) : String.Format("lastaccepted-{0}", questId);
+                if (entity.WatchedAttributes.GetDouble(key, -quest.cooldown) + quest.cooldown < sapi.World.Calendar.TotalDays
+                        && activeQuests.Find(activeQuest => activeQuest.questId == questId && activeQuest.questGiverId == entity.EntityId) == null
+                        && predecessorsCompleted(quest, player.PlayerUID))
+                {
+                    availableQuestIds.Add(questId);
+                }
+            }
+            var message = new QuestInfoMessage()
+            {
+                questGiverId = entity.EntityId,
+                availableQestIds = availableQuestIds,
+                activeQuests = activeQuests
+            };
+
+            sapi.Network.GetChannel("vsquest").SendPacket<QuestInfoMessage>(message, player.Player as IServerPlayer);
+        }
         public override WorldInteraction[] GetInteractionHelp(IClientWorldAccessor world, EntitySelection es, IClientPlayer player, ref EnumHandling handled)
         {
-            if (entity.Alive)
+            if (entity.Alive && !entity.HasBehavior<EntityBehaviorConversable>())
             {
                 return new WorldInteraction[] {
                     new WorldInteraction(){
